2030bb4004c3a8b38084c8470eea294dd60b2ff7
KarthikRao <karthik@indix.com>
2015-07-31 19:55:13 +0530

    newmodel

--- nn/python/face_detect_model.py
+++ nn/python/face_detect_model.py
@@ -10,27 +10,28 @@ from keras.utils import np_utils, generic_utils
 from six.moves import range
 from load_dataset import *
 
-faces = '/media/karthik/0ed39684-ffb3-432a-bd76-d1e04ccd8716/administrator/data/face_patches'
-nfaces = '/media/karthik/0ed39684-ffb3-432a-bd76-d1e04ccd8716/administrator/data/random_patches'
-files = load_face_patches(faces) + load_notface_patches(nfaces)
+faces = '/home/indix/karthik/facepatches'
+nfaces = '/home/indix/karthik/randompatches'
+files = load_face_patches(faces) + load_notface_patches(nfaces)[:20000]
 (train,test,labels,lblmap) = create_train_test(files)
-
+print( 'TrainSize :', len(train) )
+print( 'TestSize :', len(test) )
 
 model = Sequential()
-model.add(Convolution2D(10, 3, 5, 5, border_mode='full'))
+model.add(Convolution2D(10, 3, 5, 5, border_mode='valid'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(poolsize=(2,2)))
 
-model.add(Convolution2D(30, 10, 5, 5, border_mode='full'))
+model.add(Convolution2D(30, 10, 5, 5, border_mode='valid'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(poolsize=(2,2)))
 
-model.add(Convolution2D(30, 30, 3, 3, border_mode='full'))
+model.add(Convolution2D(30, 30, 3, 3, border_mode='valid'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(poolsize=(2,2)))
 
 model.add(Flatten())
-model.add(Dense(64*8*8, 512))
+model.add(Dense(3000, 512))
 model.add(Activation('relu'))
 
 model.add(Dense(512, len(labels)))
@@ -39,11 +40,28 @@ model.add(Activation('softmax'))
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
+mean = np.load( '/home/indix/karthik/mean.npy' )
+meanimg = np.zeros((3,100,100))
+meanimg[0] = mean[:,:,0]
+meanimg[1] = mean[:,:,1]
+meanimg[2] = mean[:,:,2]
+
 for batch in makeBatches(train,5000):
     (X,y) = read_batch(batch,lblmap)
     Y = np_utils.to_categorical(y, len(labels))
-    model.train(X,Y)
-    (X_test,y_test) = read_batch(batch,lblmap)
+    X = X.astype("float32")
+    Y = Y.astype("float32")
+    X = X/255.0
+    X = X - meanimg
+    print( 'Training..' )
+    model.fit(X,Y,batch_size=100,nb_epoch=1)
+
+    (X_test,y_test) = read_batch(test,lblmap)
     Y_test = np_utils.to_categorical(y_test, len(labels))
-    model.test(X_test,Y_test)
+    X_test = X_test.astype("float32")
+    Y_test = Y_test.astype("float32")
+    X_test = X_test/255.0
+    X_test = X_test - meanimg
+    print( 'Eval...' )
+    model.evaluate(X_test,Y_test,batch_size=100,show_accuracy=True)
     
