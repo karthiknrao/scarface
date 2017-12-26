ab9fa6424809661c9a84af5d916a1f3dd4048343
Ubuntu <ubuntu@ip-172-31-57-51.ec2.internal>
2017-12-26 07:18:53 +0000

    update

--- scripts/gmaps_download.py
+++ scripts/gmaps_download.py
@@ -3,7 +3,9 @@ import time
 import os
 import math
 import sys
-import pdb
+import cv2
+
+burl = 'http://mt0.google.com/vt/lyrs=s@110&hl=en&x=%d&s=&y=%d&z=%d&s='
 
 def xyfromlatlon( lat_deg, lon_deg, zoom ):
     lat_rad = math.radians(lat_deg)
@@ -19,6 +21,38 @@ def latlonfromxy( x, y, zoom ):
     lat_deg = math.degrees( lat_rad )
     return ( lat_deg, lon_deg )
 
+def gettilelatlon(lat,lon,zoom,destdir):
+    x,y = xyfromlatlon(float(lat),float(lon),int(zoom))
+    return gettilexy(x,y,zoom,destdir)
+
+def gettilexy(x,y,zoom,destdir):
+    url = burl % ( x, y, int(zoom) )
+    destpath = os.path.join(destdir,str(x))
+    if not os.path.exists(destpath):
+        os.mkdir(destpath)
+        fname = os.path.join(destpath,str(y)+'.jpg')
+        print(url,fname)
+        page = urllib.urlopen(url)
+        data = page.read()
+        with open(fname,'w') as outfile:
+            outfile.write(data)
+    return fname
+
+def getwindowxy(x,y,zoom,destdir,width,outname):
+    tiles = []
+    for i in range( x - width, x + width + 1):
+        for j in range( y - width, y + width + 1):
+            fname = gettilexy(i,j,zoon,destdir)
+            tiles.append(fname)
+    winsize = (2*width + 1)*256
+    buff = np.zeros((winsize,winsize,3))
+    w = 2*width + 1
+    for i in range(2*width+1):
+        for j in range(2*width+1):
+            img = cv2.imread(tiles[i*w + j])
+            buff[i*256:i*256+256,j*256:j*256+256] = img
+    cv2.imwrite(outname,buff)
+    
 if __name__ == '__main__':
     """
     root = sys.argv[1]
@@ -30,7 +64,7 @@ if __name__ == '__main__':
     root = 'maps'
     zoom = '19'
     width = 1
-    burl = 'http://mt0.google.com/vt/lyrs=s@110&hl=en&x=%d&s=&y=%d&z=%d&s='
+    
     destdir = os.path.join(root,zoom)
 
     locs = [ x.strip().split(',') for x in open(sys.argv[1]).readlines() ]
@@ -38,20 +72,8 @@ if __name__ == '__main__':
     if not os.path.exists(destdir):
         os.makedirs(destdir)
 
-    for lat, lon in locs:
+    outname = 'fullimage'
+    for i, lat, lon in enumerate(locs):
         x,y = xyfromlatlon(float(lat),float(lon),int(zoom))
-
-        for i in range( x - width, x + width + 1 ):
-            for j in range( y - width, y + width + 1 ):
-                url = burl % ( i, j, int(zoom) )
-                destpath = os.path.join(destdir,str(i))
-                if not os.path.exists(destpath):
-                    os.mkdir(destpath)
-                fname = os.path.join(destpath,str(j)+'.jpg')
-                print(url,fname)
-                page = urllib.urlopen(url)
-                data = page.read()
-                with open(fname,'w') as outfile:
-                    outfile.write(data)
-                time.sleep(2.0)
-    
+        getwindowxy(x,y,zoom,destdir,1,os.path.join(outname,str(i)+'.jpg'))
+        
